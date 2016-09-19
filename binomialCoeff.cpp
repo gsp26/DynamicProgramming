2d149db87699238fb11bf4b11766b320e2fdde5b
gsp26 <g.s.parihar@gmail.com>
2016-09-19 08:25:53 +0530

    binomialCoeff

---
+++ binomialCoeff.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+int binomialCoeff(int n, int k)
+{
+    if(k==0 || k==n)
+        return 1;
+
+
+    return binomialCoeff(n-1,k-1) + binomialCoeff(n-1,k);
+}
+
+int min(int a,int b)
+{
+    return  (a<b)?a:b;
+}
+
+
+int binomialCoeffDP(int n, int k)
+{
+    int C[n+1][k+1];
+    int i,j;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=min(i,k);j++)
+        {
+            //base cases
+            if(j==0 || j==i)
+                C[i][j] = 1;
+            else
+                C[i][j] = C[i-1][j-1] + C[i-1][j];
+        }
+    }
+
+    return C[n][k];
+}
+
+
+int main()
+{
+    int n=5,k=2;
+    cout << "\nValue of binomialCoeff for n=5 and k=2 is - " << binomialCoeff(n,k)<<endl;
+    cout << "\nValue of binomialCoeff using DP for n=5 and k=2 is - " << binomialCoeffDP(n,k)<<endl;
+
+    return 0;
+}
+
